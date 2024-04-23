3d477ece8ef0b394c030412a61ba0b16bbcc7413
Anirudh <saianirudhduggineni58@gmail.com>
2024-04-23 17:44:54 +0530

    Add Role Updated

--- src/components/User Management/AddRole.jsx
+++ src/components/User Management/AddRole.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import { useNavigate } from 'react-router-dom';
-
+import Accordion from 'react-bootstrap/Accordion';
+import axios from 'axios';
 
 function AddRole() {
-    const navigate =useNavigate();
-   
-    const handleRedirect = () => {
-      navigate("/UserManagement/UserManagementAdd");
-    }
+    const navigate = useNavigate();
 
+  const handleRedirect = () => {
+    navigate("/UserManagement/UserManagementAdd");
+  };
+
+  const [data, setData] = useState([]);
+  const [menuData, setMenuData] = useState([]);
+  const [configData, setConfigData] = useState([]);
+  const [reportData, setReportData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:5000/main_menu");
+        if (response.status !== 200) {
+          throw new Error('Failed to fetch menu data');
+        }
+        setData(response.data.main_menu);
+      } catch (error) {
+        console.error('Error fetching menu data:', error);
+      }
+    };
+  
+    fetchData();
+  
+    if (data.length > 0) {
+      const filteredMenuItems = data.filter(category => category.category === "Menu");
+      if (filteredMenuItems.length > 0) {
+        setMenuData(filteredMenuItems[0].menu_items);
+      }
+      const filteredConfigItems = data.filter(category => category.category === "Configuration");
+      if (filteredConfigItems.length > 0) {
+        setConfigData(filteredConfigItems[0].menu_items);
+      }
+      const filteredReportItems = data.filter(category => category.category === "Reports");
+      if (filteredReportItems.length > 0) {
+        setReportData(filteredReportItems[0].menu_items);
+      }
+    }
+  }, []);
+  
+  console.log("wgrfywefgugf")
   return (
       <div>
           <div className='container-fluid'>
@@ -25,12 +63,89 @@ function AddRole() {
 
               <hr />
 
+              <div className='row'>
 
+                  <div className="col-3 mb-3">
+                      <label for="empid" className="form-label m-1 fw-bold">Employee Id</label>
+                      <input type="text" id='empid' className='form-control h-50' placeholder='Enter Item Code' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="username" className="form-label m-1 fw-bold">Username</label>
+                      <input type="text" id='username' className='form-control h-50' placeholder='Enter Username' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="emailid" className="form-label m-1 fw-bold">Email ID</label>
+                      <input type="text" id='emailid' className='form-control h-50' placeholder='Enter Email Id' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="roles" className="form-label m-1 fw-bold">Role</label>
+                      <select className="form-select  form-control h-50" id="roles" placeholder="Roles" required>
+                          <option selected disabled value="">Select Production Code.</option>
+                          <option>...</option>
+                      </select>
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="password" className="form-label m-1 fw-bold">Password</label>
+                      <input type="text" id='password' className='form-control h-50' placeholder='*************' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="cnfrmpassword" className="form-label m-1 fw-bold">Confirm Password</label>
+                      <input type="text" id='cnfrmpassword' className='form-control h-50' placeholder='*************' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="lines" className="form-label m-1 fw-bold">Line</label>
+                      <select className="form-select  form-control h-50" id="lines" placeholder="Select SMT Lines" required>
+                          <option value="">Select SMT Lines.</option>
+                          <option>...</option>
+                      </select>
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="stations" className="form-label m-1 fw-bold">Stations</label>
+                      <select className="form-select  form-control h-50" id="stations" placeholder="Select Stations" required>
+                          <option value="">Select Stations</option>
+                          <option>...</option>
+                      </select>
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="phnnumber" className="form-label m-1 fw-bold">Mobile Number</label>
+                      <input type="text" id='phnnumber' className='form-control h-50' placeholder='+91 00000 00000' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="emgcontact" className="form-label m-1 fw-bold">Emergency Contact</label>
+                      <input type="text" id='emgcontact' className='form-control h-50' placeholder='+91 00000 00000' />
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="status" className="form-label m-1 fw-bold">Status</label>
+                      <select className="form-select  form-control h-50" id="status" placeholder="Select Status" required>
+                          <option value="">Select Status</option>
+                          <option>...</option>
+                      </select>
+                  </div>
+
+                  <div className="col-3 mb-3">
+                      <label for="department" className="form-label m-1 fw-bold">Select Department</label>
+                      <select className="form-select  form-control h-50" id="department" placeholder="Select Process Status" required>
+                          <option value="">Select Process Status</option>
+                          <option>...</option>
+                      </select>
+                  </div>
+
+              </div>
               <hr />
           </div>
 
           <center>
-              <div className='d-flex justify-content-center gap-4'>
+              <div className='d-flex justify-content-center gap-4 mt-5'>
                   <button className='btn btn-success rounded-3 my-3 px-5 py-1 '>Save</button>
                   <button className='btn btn-white border-secondary shadow rounded-3 my-3 px-5 py-1'>Reset</button>
               </div>
